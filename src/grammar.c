af313516b941bb5a83f13c99ce2e7795b1322428
Sarah Onfray <sarah.onfray@epita.fr>
2018-11-13 20:27:35 +0100

    grammar: test list and temporary command

--- src/grammar.c
+++ src/grammar.c
@@ -4,66 +4,12 @@
 #include "includes/queue.h"
 #include "includes/token.h"
 
-
-struct queue *grammar_check (struct queue *tokens)
+static struct queue *g_command(struct queue *tokens, struct queue *grammar)
 {
-    struct queue *grammar = init_queue();
-    int isEOF = 0;
-
-    while(tokens->size > 0)
-    {
-        struct token *tok = tokens->head->elem;
-        if(tok->type == ENDOF)
-        {
-            grammar = push(grammar, pop(tokens);
-            break;
-        }
-
-        else
-        {
-            grammar = list(tokens, grammar);
-        }
-    }
-
-    free_queue(tokens);
-    return grammar;
+    return push(grammar, pop(tokens));
 }
 
-struct queue *g_andor(struct queue *tokens, struct queue *grammar)
-{
-    if (!g_pipeline(tokens, grammar))
-    {
-        return NULL;
-    }
-
-    while (tokens->size > 0 && grammar)
-    {
-        struct token *tok = tokens->head->elem;
-        if (tok->type == LOGICAL_AND || tok->type == LOGICAL_OR)
-        {
-            grammar = push(grammar, pop(tokens));
-            struct token *tok = tokens->head->elem;
-            while (!strcmp(tok->name, '\n'))
-            {
-                grammar = push(grammar, pop(tokens));
-                if (!tokens->head)
-                    return NULL;
-                tok = tokens->head->elem;
-            }
-            grammar = g_pipeline(tokens, grammar);
-            if (!grammar)
-                return NULL;
-        }
-        else
-        {
-            break;
-        }
-    }
-
-    return grammar;
-}
-
-struct queue *g_pipeline(struct queue *tokens, struct queue *grammar)
+static struct queue *g_pipeline(struct queue *tokens, struct queue *grammar)
 {
     struct token *tok = grammar->head->elem;
     if (!strcmp("!", tok->name))
@@ -102,3 +48,97 @@ struct queue *g_pipeline(struct queue *tokens, struct queue *grammar)
     }
     return grammar;
 }
+
+static struct queue *g_andor(struct queue *tokens, struct queue *grammar)
+{
+    if (!g_pipeline(tokens, grammar))
+    {
+        return NULL;
+    }
+
+    while (tokens->size > 0 && grammar)
+    {
+        struct token *tok = tokens->head->elem;
+        if (tok->type == LOGICAL_AND || tok->type == LOGICAL_OR)
+        {
+            grammar = push(grammar, pop(tokens));
+            struct token *tok = tokens->head->elem;
+            while (!strcmp(tok->name, '\n'))
+            {
+                grammar = push(grammar, pop(tokens));
+                if (!tokens->head)
+                    return NULL;
+                tok = tokens->head->elem;
+            }
+            grammar = g_pipeline(tokens, grammar);
+            if (!grammar)
+                return NULL;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return grammar;
+}
+
+
+static struct queue *g_list(struct queue *tokens, struct queue *grammar)
+{
+    if(g_andor(tokens, grammar) == NULL)
+    {
+        return NULL;
+    }
+    while(1)
+    {
+        if(tokens->head == NULL)
+        {
+            return grammar;
+        }
+        else if((tokens->head->elem->type == SEMICOLON)||
+           (tokens->head->elem->type == AND))
+        {
+             grammar=push(grammar,pop(tokens));
+             if(tokens->head == NULL)
+             {
+                 return grammar;
+             }
+             else if(g_andor(tokens, grammar))
+             {
+                 return NULL;
+             }
+        }
+        else
+        {
+            return NULL;
+        }
+
+    }
+}
+
+struct queue *grammar_check (struct queue *tokens)
+{
+    struct queue *grammar = init_queue();
+    int isEOF = 0;
+
+    while(tokens->size > 0)
+    {
+        struct token *tok = tokens->head->elem;
+        if(tok->type == ENDOF)
+        {
+            grammar = push(grammar, pop(tokens);
+            break;
+        }
+
+        else
+        {
+            grammar = list(tokens, grammar);
+        }
+    }
+
+    free_queue(tokens);
+    return grammar;
+}
+
+
