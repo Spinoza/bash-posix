75e78139c16fde4e0b5c2e09df6f092d020e7318
Yohann Degli Esposti <yohann.degli-esposti@epita.fr>
2018-11-13 19:50:33 +0100

    grammar: list, input, andor beginning.

--- src/grammar.c
+++ src/grammar.c
@@ -4,3 +4,74 @@
 #include "includes/queue.h"
 #include "includes/token.h"
 
+
+struct queue *grammar_check (struct queue *tokens)
+{
+    struct queue *grammar = init_queue();
+    int isEOF = 0;
+
+    while(tokens->size > 0)
+    {
+        struct token *tok = tokens->head->elem;
+        if(tok->type == ENDOF)
+        {
+            grammar = push(grammar, pop(tokens);
+            break;
+        }
+
+        else
+        {
+            grammar = list(tokens, grammar);
+        }
+    }
+
+    free_queue(tokens);
+    return grammar;
+}
+
+struct queue *g_andor(struct queue *tokens, struct queue *grammar)
+{
+    if (!g_pipeline(tokens, grammar))
+    {
+        return NULL;
+    }
+
+    while (tokens->size > 0 && grammar)
+    {
+        struct token *tok = tokens->head->elem;
+        if (tok->type == LOGICAL_AND || tok->type == LOGICAL_OR)
+        {
+            grammar = push(grammar, pop(tokens));
+            struct token *tok = tokens->head->elem;
+            while (!strcmp(tok->name, '\n'))
+            {
+                grammar = push(grammar, pop(tokens));
+                if (!tokens->head)
+                    return NULL;
+                tok = tokens->head->elem;
+            }
+            grammar = g_pipeline(tokens, grammar);
+            if (!grammar)
+                return NULL;
+        }
+    }
+
+    return grammar;
+}
+
+struct queue *g_pipeline(struct queue *tokens, struct queue *grammar)
+{
+    struct token *tok = grammar->head->elem;
+    if (!strcmp("!", tok->name))
+    {
+        grammar = push(grammar, pop(tokens));
+    }
+
+    if (!g_command(tokens, grammar))
+    {
+        return NULL;
+    }
+
+
+
+}
