0d209bb20403299d1dc0b74b384a981f0b4bca82
Yohann Degli Esposti <yohann.degli-esposti@epita.fr>
2018-11-14 23:01:20 +0100

    grammar: beginning of main testing.

--- src/tests/grammar_main.c
+++ src/tests/grammar_main.c
@@ -6,41 +6,44 @@ int main(void)
     struct linked_list *l= init_link();
 
     struct token *w1 = malloc(sizeof(struct token));
-    w1->type = WORD;
+    w1->type = IF;
     w1->name = "{";
     struct token *lAN = malloc(sizeof(struct token));
     lAN->type = WORD;
     lAN->name = "bonjour";
     struct token *w2 = malloc(sizeof(struct token));
-    w2->type = WORD;
+    w2->type = LOGICAL_AND;
     w2->name = "}";
     struct token *semi = malloc(sizeof(struct token));
-    semi->type = PIPE;
+    semi->type = WORD;
     semi->name = "pipe";
     struct token *w3 = malloc(sizeof(struct token));
-    w3->type = ASSIGNMENT_W;
+    w3->type = SEMICOLON;
     w3->name = "asignment word";
     struct token *lOR = malloc(sizeof(struct token));
-    lOR->type = WORD;
+    lOR->type = THEN;
     lOR->name = "elem";
     struct token *w4 = malloc(sizeof(struct token));
-    w4->type = LOGICAL_AND;
+    w4->type = WORD;
     w4->name = "logical_and";
     struct token *WOR1 = malloc(sizeof(struct token));
-    WOR1->type = WORD;
+    WOR1->type = SEMICOLON;
     WOR1->name = "func";
     struct token *WOR2 = malloc(sizeof(struct token));
-    WOR2->type = WORD;
+    WOR2->type = ELSE;
     WOR2->name = "(";
     struct token *WOR3 = malloc(sizeof(struct token));
     WOR3->type = WORD;
-    WOR3->name = ")";
+    WOR3->name = "adazd";
     struct token *WOR4 = malloc(sizeof(struct token));
-    WOR4->type = WORD;
+    WOR4->type = SEMICOLON;
     WOR4->name = "for";
     struct token *eo = malloc(sizeof(struct token));
-    eo->type = ENDOF;
+    eo->type = FI;
     eo->name = "eof";
+    struct token *eo2 = malloc(sizeof(struct token));
+    eo2->type = ENDOF;
+    eo2->name = "eof";
 
     add(l, w1);
     add(l, lAN);
@@ -54,6 +57,7 @@ int main(void)
     add(l, WOR3);
     add(l, WOR4);
     add(l, eo);
+    add(l, eo2);
 
     struct nL *head = l->head;
     while(head)
