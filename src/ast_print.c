3b23743ada8d40dec17ad99e37f6f4ca04f7059e
Yohann Degli Esposti <yohann.degli-esposti@epita.fr>
2018-11-15 21:46:16 +0100

    ast_print: corrected a bugpreventing same names to appear differently on tree.

--- src/ast_print.c
+++ src/ast_print.c
@@ -2,13 +2,14 @@
 #include "ast.h"
 #include <err.h>
 
-void print_aux(FILE *f, struct node *ast)
+
+void print_aux(FILE *f, struct node *ast, int i)
 {
     struct node *as = ast->children;
     while(as)
     {
-        fprintf(f, "    %s -- %s\n", ast->instr, as->instr);
-        print_aux(f, as);
+        fprintf(f, "    %s_%i -- %s_%i\n", ast->instr, i-1, as->instr, i);
+        print_aux(f, as, i+1);
         as = as->next;
     }
 }
@@ -18,8 +19,8 @@ void print_ast(struct node *ast)
     FILE *file = fopen("ast.dot", "w+");
     if(!file)
         errx(-1, "42sh:print_ast: fail open file");
-    fprintf(file,"graph ast\n{\n    ");
-    print_aux(file,ast);
+    fprintf(file,"graph ast\n{\n");
+    print_aux(file,ast,1);
     fprintf(file,"}");
     fclose(file);
 }
