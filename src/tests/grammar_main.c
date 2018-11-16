c1f42e4cfaf743dab224800475e220f2102adbf0
Yohann Degli Esposti <yohann.degli-esposti@epita.fr>
2018-11-16 15:19:08 +0100

    grammar: beginning of tests again.

--- src/tests/grammar_main.c
+++ src/tests/grammar_main.c
@@ -3,8 +3,22 @@
 #include "lexer.h"
 #include "ast.h"
 
+
 int main(int argc, char *argv[])
 {
     struct linked_list *l = lexer(argv, argc);
-    return grammar_check(l);
+    struct nL *n = l->head;
+    while (n)
+    {
+        printf("%s->", n->elem->name);
+        n = n->next;
+    }
+    printf("\n");
+    int a = grammar_check(l);
+    printf("%i \n", a);
+
+    struct node *ast = build_ast(l);
+    print_ast(ast);
+
+    return 1;
 }
