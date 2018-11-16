a160fee46718a7bf51fcfd1312e55c3349761b74
Yohann Degli Esposti <yohann.degli-esposti@epita.fr>
2018-11-16 14:59:13 +0100

    42sh: main function on work.

--- src/42sh.c
+++ src/42sh.c
@@ -5,10 +5,15 @@
 #include "token.h"
 #include "linked_list."
 #include "ast.h"
+#include "execution_ast.h"
+#include "err.H"
 
 int main(int argc, char *argv[])
 {
     struct linked_list *tokens = lexer(argv, argc);
     int isgramm = grammar_check(tokens);
+    if (!isgramm)
+        errx(1, "Lexer error. Is your input conform to grammar ?");
+    struct node *ast = ast_build(tokens);
     return isgramm;
 }
