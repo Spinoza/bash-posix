5739da95e62e55dbe39ce689b9d415a854c0e46c
Salome Desauty <salome.desauty@epita.fr>
2018-11-16 15:52:44 +0100

    [EXEC_AST] implementing if

--- src/execution_ast.c
+++ src/execution_ast.c
@@ -121,7 +121,9 @@ struct node *get_oper_node(struct node *start)
     struct node *iter = start;
     for (; iter; iter = iter->next)
     {
-        if (iter->tokentype == LOGICAL_AND || iter->tokentype == LOGICAL_OR)
+        if (iter->tokentype == LOGICAL_AND
+            || iter->tokentype == LOGICAL_OR
+            || iter->tokentype == SEMICOLON)
             return iter;
     }
     return NULL;
@@ -142,35 +144,13 @@ int if_cond(struct node *n)
         char *oper = oper_node->instr;
         if ((!strcmp(oper,"&&") && !res) || (!strcmp(oper,"||") && res))
             break;
+        if (oper_node->tokentype == SEMICOLON)
+            break;
         iter = oper_node->next;
     }
     return res;
 }
-/*
-int while_cond(struct node *n)
-{
-    struct node *condition = n->children[0];
-    int res = 0;
-    for (size_t i = 0; i < condition->nbchild;)
-    {
-        int index = get_oper_node(condition,i + 1);
-        if (index == - 1)
-            index = parent->nbchild;
-        size_t ind = index;
-        char *string = to_execute(parent, i, ind);
-        res = exec_command(string);
-        if (index == -1)
-            break;
-        char *oper = condition->children[index]->instr;
-        if ((!strcmp(oper,"&&") && res) || (!strcmp(oper,"||") && !res))
-        {
-            res = exec_command(condition->children[index + 1]);
-        }
-        i = index + 2;
-    }
-    return res;
-}
-*/
+
 int traversal_ast(struct node *n)
 {
 
@@ -180,44 +160,42 @@ int traversal_ast(struct node *n)
         if (n->type == A_INSTRUCT)
         {
             struct node *oper_node = get_oper_node(n);
-            char *oper = "";
-            if (oper_node)
-            {
-                oper = oper_node->instr;
-            }
+            char *oper = (oper_node ? oper_node->instr : "");
             char **command_call = to_execute(n, oper_node);
             int rvalue = exec_command(command_call);
+            free(command_call);
             if ((!strcmp(oper,"&&") && !rvalue)
                 || (!strcmp(oper,"||") && rvalue))
                 return traversal_ast(oper_node->next);
-
-            free(command_call);
+            if (!strcmp(oper, ";"))
+                return traversal_ast(oper_node->next);
         }
         if (n->type == A_IF)
         {
             res = if_cond(n);
             if (res == 0)
-                traversal_ast(n->children->next);
-            else if (res == 1) //if res != 1, the exec returned an error
-                traversal_ast(n->children->next->next);
+                return traversal_ast(n->children->next);
+            else //if res != 0, the exec returned an error
+                return traversal_ast(n->children->next->next);
         }
-        return res;
-        /*
         if (n->type == A_WHILE)
         {
-            res = while_cond(n);
-        }
-        if (n->type == A_FOR)
-            return for_cond(n);*/
+            res = if_cond(n);
+            if (res == 0)
+            {
+                while (if_cond(n) == 0)
+                    res = traversal_ast(n->children->next);
+            }
+            return res;//return value ?
+
+        }/*
+            if (n->type == A_FOR)
+            {
+
+            }*/
     }
     struct node *iter = n->children;
-    /*for (; iter; iter = iter->next)
-    {
-        int r = traversal_ast(iter);
-        if (r != 0)
-            return r;
-    }*/
-    return traversal_ast(iter); //FIX RETURN VALUE
+    return traversal_ast(iter);
 }
 
 int execution_ast(struct node *n)
