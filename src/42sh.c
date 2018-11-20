14d327c84fcdfb2f7437b695a81e3120ecffaa35
Yohann Degli Esposti <yohann.degli-esposti@epita.fr>
2018-11-20 17:17:59 +0100

    added a beginning of PS1 implementation.

--- src/42sh.c
+++ src/42sh.c
@@ -1,4 +1,9 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <libgen.h>
 #include "lexer.h"
 #include "grammar_check.h"
 #include "options.h"
@@ -7,6 +12,38 @@
 #include "ast.h"
 #include "execution_ast.h"
 #include "err.h"
+
+static size_t mstrlen(char *str)
+{
+    return *str ? 1 + strlen(str + 1) : 0;
+}
+
+static int interactive_mode(struct option *options)
+{
+    while (isatty(STDIN_FILENO))
+    {
+        char *line;
+        size_t n = 0;
+        printf("%s$ ", basename("/build/42sh"));
+        getline(&line, &n, stdin);
+        size_t i = mstrlen(line);
+        line[i - 1] = '\0';
+        struct linked_list *tokens = lexer_c(line);
+        int isgramm = grammar_check(tokens);
+        if (!isgramm)
+        {
+            fprintf(stdout, "lexer error: Is your input conform to grammar ?\n");
+        }
+        else
+        {
+            struct node *ast = build_ast(tokens);
+            if (options->ast_print == TRUE)
+                print_ast(ast);
+            execution_ast(ast);
+        }
+    }
+    return 1;
+}
 
 int main(int argc, char *argv[])
 {
@@ -19,6 +56,10 @@ int main(int argc, char *argv[])
         printf("Version 0.3\n");
         free(options);
         return 0;
+    }
+    if (argc == 1 || options->c == FALSE)
+    {
+        return interactive_mode(options);
     }
     struct linked_list *tokens;
     if (options->c == TRUE)
