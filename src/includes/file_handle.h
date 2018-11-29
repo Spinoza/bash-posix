6134e0b46c177ca6af505e5d09cb88596a6b6a8f
Sarah Onfray <sarah.onfray@epita.fr>
2018-11-29 12:37:49 +0100

    [DOC] started commenting file_handle.h

--- src/includes/file_handle.h
+++ src/includes/file_handle.h
@@ -3,7 +3,26 @@
 
 #include "linked_list.h"
 
+/**
+  *\file filehandle.h
+  *\brief
+  *Handle loading, opening and closing of files
+  */
+
+
+/**
+  *\fn int is_file(char *path)
+  *\brief CHecks wheather the path arguments is a file
+  *\param Char *path is the path to check
+  *\return an 1 if everything works properly, 0 otherwise
+  */
 int is_file(char *path);
+
+/**
+  *\fn struct linked_list *read_fil(char *path)
+  *\brief handles the file passed in argument
+  *\return a struct linked_list if everything works properly, NULL otherwise
+  */
 struct linked_list *read_fil(char *path);
 void fuse_lists(struct linked_list *ll, char *string);
 
