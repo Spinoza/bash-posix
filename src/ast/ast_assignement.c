44a731658d7e0a68a015a418a948f61ee46070b5
Salome Desauty <salome.desauty@epita.fr>
2018-11-30 17:10:26 +0100

    [ASSIGN] assignment with hashmap done but not tested

--- src/ast/ast_assignement.c
+++ src/ast/ast_assignement.c
@@ -4,28 +4,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct a_tab *init_assignment(void)
+struct assignment **init_assignment(void)
 {
-    struct a_tab *a_tab = malloc(sizeof(struct a_tab));
-    a_tab->a_list = malloc(HASH_TAB_SIZE * sizeof(struct assignement *));
+    struct assignment **a_tab = calloc(HASH_TAB_SIZE, sizeof(struct assignment *));
     return a_tab;
 }
 
+//return the position in the hash table
 int hash_function(char *string)
 {
     float r = 0;
     int i = 0;
-    for ( ; *(string + i); i++)
+    for ( ; *(string + i) && *(string + i) != '='; i++)
         r+= ((float)string[i] * TETA ) % HASH_TAB_SIZE;
     return (int)(r / i);
 }
 
 struct assignment *to_assign(char *string)
 {
-    struct assignment *new = malloc(sizeof(struct assignment));
+    struct assignment *new = calloc(1, sizeof(struct assignment));
     int i = 0;
     int len = strlen(string);
-    for (; *(string + i) && *(string + i) != '='; i++);
+    for (; *(string + i) && *(string + i) != '='; i++)
+        continue;
     new->name = malloc(sizeof(char) * i);
     new->name = memcpy(new->name, string, i);
     new->name[i] = '\0';
@@ -33,53 +34,63 @@ struct assignment *to_assign(char *string)
     new->value[len - i] = '\0';
     return new;
 }
-int already_exists(struct assignment *to_assign, struct tab_a *tab)
+
+int same_name(char *a, char *b)
 {
-    char *string = to_assign->name;
-    int name_len = strlen(string) + 1;
-    int value_len = strlen(to_assign->name) +1;
-    for (int i =0; i < nb; i++) //Value already present
+    int i = 0;
+    for ( ;  a[i] && a[i] != '=' && b[i] && a[i] == b[i]; i++)
+        continue;
+    if (!b[i] && a[i] == '=')
+        return 1;
+    return 0;
+}
+
+int already_exists(char *name, struct assignement **a_tab, int pos)
+{
+    struct assignment *a = a_tab[pos];
+    for (; a; a = a->next)
     {
-        if (!strcmp(string, tab->assignment->name))
+        if (same_name(string, a->name))
         {
-            free(tab->value);
-            tab->value = malloc(sizeof(char) * value_len);
-            tab->value = memcpy(tab->value, to_assign->value, value_len);
-            free(to_assign);
+            free(a->value);
+            for (; *(string + i) && *(string + i) != '='; i++)
+                continue;
+            size_t value_len = strlen(string) + 1;
+            a->value = calloc(sizeof(char), value_len);
+            tab->value = memcpy(tab->value, string, value_len);
             return 1;
         }
     }
     return 0;
 }
-void add_assignment(struct assignment *to_assign, struct tab_a *tab)
+struct assignement **add_assignment(char *string,
+        struct assignment **a_tab)
 {
-    char *string = to_assign->name;
     int name_len = strlen(string) + 1;
-
-    if (already_exists(to_assign, tab)) //just change the value field
-        return;
-    if (tab->nb == tab->capacity)
+    int pos = hash_function(name);
+    if (already_exists(string, a_tab, pos))
+        return a_tab;
+    struct assignment *new = to_assign(string);
+    struct assignement *a = a_tab[pos];
+    if (a == NULL)
     {
-        capacity *=2;
-        void *tmp = realloc(tab->assignment,sizeof(struct assignment)
-                * capacity);
-        if (!tmp)
-        {
-            capacity /=2;
-            fprintf(stderr,"42sh : Out of memory");
-            return;
-        }
-        tab->assignment = tmp;
+        a_tab[pos] = new;
+        return a_tab;
     }
-    tab->assignment[tab->nb++] = to_assign;
+    for (; a && a->next; a = a ->next)
+        continue;
+    a->next = new;
+    return a_tab;
 }
 
-char *get_assign(char *name, struct tab_a **tab)
+char *get_assign(char *name, struct assignment **a_tab)
 {
-    for (int i = 0; i < la->nb; i++)
+    int pos = hash_function(name);
+    struct assignement *a = a_tab[pos];
+    for ( ; a ; a = a ->next)
     {
-        if (!strcmp(name, tab->assignment[i]->name))
-            return tab->assignment[i]->value;
+        if (!strcmp(name, a->name))
+            return a->value;
     }
-    return name;
+    return "";
 }
