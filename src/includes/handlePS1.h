2144d9a6b5f8909d0a700f4f2d901feadaf91bfd
Yohann Degli Esposti <yohann.degli-esposti@epita.fr>
2018-12-04 12:06:50 +0100

    [PROMPT] added variables PS1 and PS2 to initialization of assignment table. Added function get_assignment, returning the value of an assignment in the table.

--- src/includes/handlePS1.h
+++ src/includes/handlePS1.h
@@ -10,12 +10,13 @@
   *while in tty
   */
 
-
+char *get_assignment(char *name);
 /**
   *\fn struct nL *handletty(void)
   *\brief handles everything that happens in the prompt
   *\return a Struct nL if everything works properly, NULL otherwise
   */
 struct nL *handletty(void);
+
 
 #endif /* ! ATTY_H */
--- src/prompt/handlePS1.c
+++ src/prompt/handlePS1.c
@@ -12,7 +12,12 @@ struct nL *handletty(void)
 {
     while (isatty(STDIN_FILENO))
     {
-        char *line = readline("> ");
+        char *line;
+        char *PS2 = get_assignment("PS2");
+        if (!PS2)
+            line = readline("> ");
+        else
+            line = readline(PS2);
         struct linked_list *tokens = lexer_c(line);
         struct nL *ret = tokens->head;
         free(tokens);
