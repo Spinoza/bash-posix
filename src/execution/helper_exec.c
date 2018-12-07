27aaae773bf90645ce06588fa61947086a9616ba
Salome Desauty <salome.desauty@epita.fr>
2018-12-07 17:34:10 +0100

    break n implemented

--- src/execution/helper_exec.c
+++ src/execution/helper_exec.c
@@ -30,7 +30,8 @@ void free_data(struct stored_data *data)
     {
         free(data->builtins);
     }
-    free(data->parent_list);
+    if (data->nbparent > 0)
+        free(data->parent_list);
     free_assignments(data->var_tab);
     free_assignments(data->alias_tab);
     free_assignments(data->export_tab);
